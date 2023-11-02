d0d294d75453bfd6fa7322e4c449eadf83c0e7f8
G Nikitha Nayak <70960751+GNikithaNayak@users.noreply.github.com>
2023-11-02 18:47:13 +0530

    Challenge 1 Updated

---
+++ Problem Solving/Day 1/Solve Me First.py
@@ -0,0 +1,21 @@
+/*CHALLENGE:
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Function Description:
+Complete the solveMeFirst function in the editor below.
+solveMeFirst has the following parameters:
+  int a: the first value
+  int b: the second value
+Returns
+  int: the sum of a and b 
+*/
+
+def solveMeFirst(a,b):
+  # Hint: Type return a+b below
+  return a + b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
