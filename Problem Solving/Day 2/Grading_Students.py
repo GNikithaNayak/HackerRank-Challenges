e292ad413c8adc44a4daaaf58cf4294f61af4f4b
G Nikitha Nayak <70960751+GNikithaNayak@users.noreply.github.com>
2023-11-03 21:17:31 +0530

    Update Grading_Students.py

--- Problem Solving/Day 2/Grading_Students.py
+++ Problem Solving/Day 2/Grading_Students.py
@@ -1,25 +1,25 @@
-/*** 
-CHALLENGE:
-HackerLand University has the following grading policy:
-  a. Every student receives a grade in the inclusive range from 0 to 100.
-  b. Any grade less than 40 is a failing grade.
-Sam is a professor at the university and likes to round each student's grade according to these rules:
-  a.If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
-  b. If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
-Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.
+# /*** 
+# CHALLENGE:
+# HackerLand University has the following grading policy:
+#   a. Every student receives a grade in the inclusive range from 0 to 100.
+#   b. Any grade less than 40 is a failing grade.
+# Sam is a professor at the university and likes to round each student's grade according to these rules:
+#   a.If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+#   b. If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+# Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.
 
-Function Description:
-Complete the function gradingStudents in the editor below.
-gradingStudents has the following parameter(s):
-  int grades[n]: the grades before rounding
+# Function Description:
+# Complete the function gradingStudents in the editor below.
+# gradingStudents has the following parameter(s):
+#   int grades[n]: the grades before rounding
 
-Returns:
-int[n]: the grades after rounding as appropriate
+# Returns:
+# int[n]: the grades after rounding as appropriate
 
-Input Format:
-The first line contains a single integer, n, the number of students.
-Each line i of the n subsequent lines contains a single integer, grades[i].
-***/
+# Input Format:
+# The first line contains a single integer, n, the number of students.
+# Each line i of the n subsequent lines contains a single integer, grades[i].
+# ***/
 
 #!/bin/python3
 
