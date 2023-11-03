e0ec8f671a718e5e05697f9a6138bbc16ec852c4
G Nikitha Nayak <70960751+GNikithaNayak@users.noreply.github.com>
2023-11-03 21:31:23 +0530

    Create Between_Two_Sets.py

---
+++ Problem Solving/Day 2/Between_Two_Sets.py
@@ -0,0 +1,76 @@
+# /***
+# CHALLENGE:
+# There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
+#   The elements of the first array are all factors of the integer being considered
+#   The integer being considered is a factor of all elements of the second array
+# These numbers are referred to as being between the two arrays. Determine how many such numbers exist.
+
+# Function Description:
+# Complete the getTotalX function in the editor below. It should return the number of integers that are betwen the sets.
+# getTotalX has the following parameter(s):
+#   int a[n]: an array of integers
+#   int b[m]: an array of integers
+
+# Returns:
+# int: the number of integers that are between the sets
+
+# Input Format:
+# The first line contains two space-separated integers, n and m, the number of elements in arrays a and b.
+# The second line contains n distinct space-separated integers a[i] where 0<=i<n.
+# The third line contains m distinct space-separated integers b[j] where 0<=j<m.
+# ***/
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+from functools import reduce
+from math import gcd
+
+def lcm(x, y):
+    return (x*y) // gcd(x,y)
+    
+def getTotalX(a, b):
+    # Write your code here
+    l = reduce(lcm, a, 1)
+    g = reduce(gcd, b)
+    temp = l
+    count = 0
+
+    while l <= g:
+        if(g % l) == 0:
+            count += 1
+        l = l + temp
+
+    return count
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
+
