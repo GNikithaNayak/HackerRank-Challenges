3e1354c267d65e625b145bcf5bcf39315823b49a
G Nikitha Nayak <70960751+GNikithaNayak@users.noreply.github.com>
2023-11-04 19:30:48 +0530

    Update Forming_a_Magic_Square.py

--- Problem Solving/Day 3/Forming_a_Magic_Square.py
+++ Problem Solving/Day 3/Forming_a_Magic_Square.py
@@ -1,18 +1,18 @@
-/***
-CHALLENGE:
-We define a magic square to be an nxn matrix of distinct positive integers from 1 to n^2 where the sum of any row, column, or diagonal of length n is always equal to the same number: the magic constant.
+# /***
+# CHALLENGE:
+# We define a magic square to be an nxn matrix of distinct positive integers from 1 to n^2 where the sum of any row, column, or diagonal of length n is always equal to the same number: the magic constant.
 
-Function Description:
-Complete the formingMagicSquare function in the editor below.
-formingMagicSquare has the following parameter(s):
-int s[3][3]: a 3x3 array of integers
+# Function Description:
+# Complete the formingMagicSquare function in the editor below.
+# formingMagicSquare has the following parameter(s):
+# int s[3][3]: a 3x3 array of integers
 
-Returns:
-int: the minimal total cost of converting the input square to a magic square
+# Returns:
+# int: the minimal total cost of converting the input square to a magic square
 
-Input Format:
-Each of the 3 lines contains three space-separated integers of row s[i].
-***/
+# Input Format:
+# Each of the 3 lines contains three space-separated integers of row s[i].
+# ***/
 
 #!/bin/python3
 
@@ -32,29 +32,26 @@ import sys
     
 def formingMagicSquare(s):
     # Write your code here
-    # List of all possible 3x3 magic squares
-    magic_squares = [
-        [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
-        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
-        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
-        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
-        [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
-        [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
-        [[6, 5, 4], [1, 5, 9], [8, 3, 2]],
-        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
-    ]
-    
-    min_cost = float('inf')
-    
-    # Check the cost for each magic square and find the minimum cost
-    for magic_square in magic_squares:
-        cost = 0
-        for i in range(3):
-            for j in range(3):
-                cost += abs(magic_square[i][j] - s[i][j])
-        min_cost = min(min_cost, cost)
-    
-    return min_cost
+    mylist =[
+            [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+            [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+            [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+            [[2, 9, 4], [7, 5, 3], [6, 1, 8]], 
+            [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+            [[4, 3, 8], [9, 5, 1], [2, 7, 6]], 
+            [[6, 7, 2], [1, 5, 9], [8, 3, 4]], 
+            [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+            ]
+
+    totals = []
+    for p in mylist:
+            total = 0
+            for p_row, s_row in zip(p, s):
+                for i, j in zip(p_row, s_row):
+                    if not i == j:
+                        total += max([i, j]) - min([i, j])
+            totals.append(total)
+    return(min(totals))
 
     
 if __name__ == '__main__':
@@ -70,4 +67,5 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
 
